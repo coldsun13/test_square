a63aec5836a06e120b024ba2ecab39cf0c20bd45
Tsimafeyeu <Ihar.Tsimafeyeu@ibar.int>
2026-01-05 19:29:48 +0300

    create DI

---
+++ square_test/CoreModule/AppNetworking/DI/DependencyContainer.swift
@@ -0,0 +1,94 @@
+//
+//  DependencyContainer.swift
+//  square_test
+//
+//  Created by Ihar Tsimafeyeu on 5.01.26.
+//
+
+import Foundation
+import Swinject
+
+public func resolve<T>(
+    dependency: T.Type = T.self,
+    named name: String = "Default"
+) -> T {
+    DependencyContainer.resolve(
+        dependency: dependency,
+        named: name
+    )
+}
+
+func register<T>(
+    dependency: T.Type = T.self,
+    name: String = "Default",
+    implementation: @escaping () -> T
+) {
+    DependencyContainer.register(
+        .standard,
+        dependency: dependency,
+        name: name,
+        implementation: implementation
+    )
+}
+
+private enum DependencyBehaviour {
+    case standard
+    case singleton
+}
+
+enum DependencyContainer {
+    private static var container = Container()
+    
+    // MARK: - Setup
+    
+    static func setup(container: Container? = nil) {
+        if let inputContainer = container {
+            self.container = inputContainer
+        }
+        
+        registerHelpers()
+        registerServices()
+    }
+    
+    // MARK: - Dependencies Registration
+    
+    private static func registerHelpers() {
+        register {
+            URLParameterEncoder() as URLParameterEncoderProtocol
+        }
+        
+        register {
+            JSONParameterEncoder() as JSONParameterEncoderProtocol
+        }
+    }
+    
+    private static func registerServices() {
+
+    }
+    
+    // MARK: - Dependencies Managing
+    
+    fileprivate static func register<T>(
+        _ behaviour: DependencyBehaviour = .standard,
+        dependency: T.Type = T.self,
+        name: String = "Default",
+        implementation: @escaping () -> T
+    ) {
+        container.register(dependency, name: name) { _ in
+            implementation()
+        }.inObjectScope(
+            behaviour == .singleton
+            ? .container
+            : .transient
+        )
+    }
+    
+    fileprivate static func resolve<T>(dependency: T.Type, named name: String) -> T {
+        guard let service = container.synchronize()
+            .resolve(dependency, name: name) else {
+            fatalError("Service isn't registered")
+        }
+        
+        return service
+    }
+}
