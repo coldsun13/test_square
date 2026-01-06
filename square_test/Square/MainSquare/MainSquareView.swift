038f81c639c855e7ec1c19f0556adabf9e147cfa
Tsimafeyeu <Ihar.Tsimafeyeu@ibar.int>
2026-01-06 15:35:07 +0300

    add identifier for testing

--- square_test/Square/MainSquare/MainSquareView.swift
+++ square_test/Square/MainSquare/MainSquareView.swift
@@ -23,7 +23,6 @@ struct MainSquareView: View {
             LazyVStack(alignment: .leading, spacing: 0) {
                 ForEach(store.state.items) { item in
                     MainSquareRepoRowView(item: item)
-                        .accessibilityIdentifier("repo_cell_\(item.id)")
                     Divider()
                 }
             }
