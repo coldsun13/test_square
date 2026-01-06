f425ede3ade911259747d0ec5108481083b73427
Tsimafeyeu <Ihar.Tsimafeyeu@ibar.int>
2026-01-06 10:02:25 +0300

    create service for networking

---
+++ square_test/MainSquare/Network/Services/MainSquareNetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  MainSquareNetworkService.swift
+//  square_test
+//
+//  Created by Ihar Tsimafeyeu on 6.01.26.
+//
+
+import Foundation
+
+final class MainSquareNetworkService: NetworkService<MainSquareEndPoints>, MainSquareNetworkServiceProtocol {
+    
+}
