66281512162cc6afdc79ff1ac29aa62d1030231a
Tsimafeyeu <Ihar.Tsimafeyeu@ibar.int>
2026-01-06 13:54:13 +0300

    implement view item

---
+++ square_test/Square/MainSquare/Models/MainSquareViewItem.swift
@@ -0,0 +1,14 @@
+//
+//  MainSquareViewItem.swift
+//  square_test
+//
+//  Created by Ihar Tsimafeyeu on 6.01.26.
+//
+
+import Foundation
+
+struct MainSquareRepoViewItem: Identifiable {
+    let id: Int
+    let title: String
+    let subtitle: String
+}
