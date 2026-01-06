7f691b84cdceb6be5b40fcf505932066e732ea0b
Tsimafeyeu <Ihar.Tsimafeyeu@ibar.int>
2026-01-06 13:50:21 +0300

    remove not needed methods

--- square_test/CoreModule/AppNetworking/Service/NetworkService.swift
+++ square_test/CoreModule/AppNetworking/Service/NetworkService.swift
@@ -40,48 +40,25 @@ class NetworkService<EndPoint: EndPointType>: @unchecked Sendable {
         }
     }
 
-    func fetch(endPoint: EndPoint) async throws -> Data {
-        let response = try await router.request(endPoint)
+    func fetch<T: Decodable>(endPoint: EndPoint) async throws(NetworkError) -> T {
+        do {
+            let response = try await router.request(endPoint)
 
-        guard let data = response.data else {
-            throw NetworkError(type: .noData)
-        }
-
-        return data
-    }
-
-    // MARK: - Decodable
-
-    @discardableResult
-    func fetchDecodable<T: Decodable>(
-        endPoint: EndPoint,
-        completion: @escaping CompletionHandler<T>
-    ) -> Cancellable? {
-
-        router.request(endPoint) { result in
-            switch result {
-            case .success(let response):
-                let result: Result<T, NetworkError> =
-                    response.responseDecodable(of: T.self)
-                completion(result)
-
-            case .failure(let error):
-                completion(.failure(error))
+            if let status = response.status, !(200...299).contains(status) {
+                throw NetworkError(type: .failed)
             }
-        }
-    }
 
-    func fetchDecodable<T: Decodable>(
-        endPoint: EndPoint
-    ) async throws -> T {
+            switch response.responseDecodable(of: T.self) {
+            case .success(let model):
+                return model
+            case .failure(let error):
+                throw error
+            }
 
-        let response = try await router.request(endPoint)
-
-        switch response.responseDecodable(of: T.self) {
-        case .success(let model):
-            return model
-        case .failure(let error):
+        } catch let error as NetworkError {
             throw error
+        } catch {
+            throw NetworkError(error: error)
         }
     }
 }
